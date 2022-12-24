4ad7c34c51bd8627e10705509590c3d115e537b5
blacksilla <afonsosilva1@ua.pt>
2022-12-24 01:31:29 +0000

    Added Ex6.cs

---
+++ Ex6.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics.Tracing;
+using System.Globalization;
+
+namespace ExerciciosPraticos2
+{
+    internal class Ex6
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Função que escreve todos os números multiplos de 19 até 2000");
+
+            
+            escreveNumeros();
+        
+        }
+
+
+        static void escreveNumeros()
+        {
+            Console.WriteLine("Os múltiplos de 19 são");
+
+            for (int i = 19; i < 2000; i=i+19)
+            {
+
+                Console.Write(i + " ");
+            }
+
+        }
+    }
+}
